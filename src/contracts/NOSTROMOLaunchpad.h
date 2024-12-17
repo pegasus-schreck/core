95a1bab657ec6b41fcefa9f52c811a5e662b9e63
Bill Schreckenstein <schreck@pegasusdata.tech>
2024-12-17 06:57:44 -0600

    fixing compile errors

--- src/contracts/NOSTROMOLaunchpad.h
+++ src/contracts/NOSTROMOLaunchpad.h
@@ -171,11 +171,11 @@ public:
         projectFinance financial;
     };
 
-//    PUBLIC_PROCEDURE_WITH_LOCALS(getProject)
-//        output.status = NONE;
-//    -
+    PUBLIC_PROCEDURE_WITH_LOCALS(getProject)
+        output.status = NONE;
+    -
 
-
+    
 	REGISTER_USER_FUNCTIONS_AND_PROCEDURES    
         REGISTER_USER_PROCEDURE(createProject, 1)
 	_
